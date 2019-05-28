554151c301e7eb976d31cc0b85eabda41d12612d
Alexander Schorn <Alexander.Schorn@gmx.net>
2019-05-28 11:52:53 +0200

    Add Important Gui message

--- Gui.py
+++ Gui.py
@@ -8,16 +8,23 @@ windowY = 700
 window.title("Attention Assistnace")
 window.geometry("%dx%d" % (windowX, windowY))
 
+def openNewWindow():
+    lbl.configure(text = "Warum klickst du auf Fertig?? Du bist doch noch nicht fertig!!! :(")
+
+
 lbl = Label(window, text="Text", font=("Arial Bold", 10))
 lbl.pack(side=TOP)
 
-btnFin = Button(window, text="Fertig", command=window.quit)
+btnFin = Button(window, text="Fertig", command=openNewWindow)
 
-btnFin.place(x=windowX - 50,y=windowY - 30)
+btnFin.place(x=windowX - 50, y=windowY - 30)
 
 btnStart = Button(window, text="Start")
 
-btnStart.place(x=10,y=windowY - 30)
+btnStart.place(x=10, y=windowY - 30)
+
+
+
 
 
 window.mainloop()
