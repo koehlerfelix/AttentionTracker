e1da3ce6968c2afdc6aaff0a47a8d6d5d401b114
Felix <koehlerfelix@arcor.de>
2019-05-29 11:52:59 +0200

    add case for no connected tracker

--- src/eyetracker/connection.py
+++ src/eyetracker/connection.py
@@ -1,9 +1,11 @@
 import tobii_research as tr
 import time
-import src.eyetracker.calibration as ca
 
 found_eyetrackers = tr.find_all_eyetrackers()
 
+if len(found_eyetrackers) == 0:
+    print("No eyetrackers connected")
+    exit()
 
 my_eyetracker = found_eyetrackers[0]
 print("Address: " + my_eyetracker.address)
@@ -12,11 +14,11 @@ print("Name (It's OK if this is empty): " + my_eyetracker.device_name)
 print("Serial number: " + my_eyetracker.serial_number)
 
 def gaze_data_callback(gaze_data):
-    # Print gaze points of left and right eye
+    #Print gaze points of left and right eye
     print("Left eye: ({gaze_left_eye}) \t Right eye: ({gaze_right_eye})".format(
         gaze_left_eye = gaze_data['left_gaze_point_on_display_area'],
         gaze_right_eye = gaze_data['right_gaze_point_on_display_area']))
 
 my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
-time.sleep(5)
+time.sleep(7)
 my_eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_data_callback)
