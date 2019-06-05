787614a21faad7756dca9b131ce068a2ebcca94d
Alexander Schorn <Alexander.Schorn@gmx.net>
2019-06-05 14:33:37 +0200

    organise program

--- src/UI/Gui.py
+++ src/UI/Gui.py
@@ -1,71 +1,84 @@
 from tkinter import *
+
 import threading
 import src.eyetracker.connection as c
 import tobii_research as tr
 import src.eyetracker.eyetracker as et
 
-connected = False
+class GUI:
 
-window = Tk()
+    def run(self):
 
-windowX = 1500
-windowY = 750
+        connected = False
 
-window.title("Attention Assistnace")
-window.state('zoomed')
-window.geometry("%dx%d+0+0" % (windowX, windowY))
+        window = Tk()
 
-def openNewWindow():
-    lbl.configure(text = "Warum klickst du auf Fertig?? Du bist doch noch nicht fertig!!! :(")
+        windowX = 1500
+        windowY = 750
 
-def start_Collecting():
-    if connected:
-        thread.start()
+        window.title("Attention Assistnace")
+        window.state('zoomed')
+        window.geometry("%dx%d+0+0" % (windowX, windowY))
 
-def finish_Collecting():
-    if connected:
-        thread.join(1)
-        c.stop_collecting()
-        print("Alles fertig!")
+        thread = threading.Thread(target=self.thread_work)
 
-def thread_work():
-    if connected:
-        while (1):
-            c.start_collecting()
+        lbl = Label(window, text="Text", font=("Arial Bold", 10))
 
-def connect():
-    found_eyetrackers = tr.find_all_eyetrackers()
+        lbl.pack(side=TOP)
 
-    if len(found_eyetrackers) == 0:
-        print("No eyetrackers connected")
-        # exit()
-    else:
-        my_eyetracker = found_eyetrackers[0]
-        print("Address: " + my_eyetracker.address)
-        print("Model: " + my_eyetracker.model)
-        print("Name (It's OK if this is empty): " + my_eyetracker.device_name)
-        print("Serial number: " + my_eyetracker.serial_number)
-        global eyetracker
-        eyetracker = et.Eyetracker(my_eyetracker)
-        global connected
-        connected = True
+        btnFin = Button(window, text="Fertig", command=self.finish_Collecting(thread))
 
-thread = threading.Thread(target=thread_work)
+        btnFin.place(x=windowX - 50, y=windowY - 30)
 
-lbl = Label(window, text="Text", font=("Arial Bold", 10))
-lbl.pack(side=TOP)
+        btnStart = Button(window, text="Start", command=self.start_Collecting(thread))
 
-btnFin = Button(window, text="Fertig", command=finish_Collecting)
+        btnStart.place(x=10, y=windowY - 30)
 
-btnFin.place(x=windowX - 50, y=windowY - 30)
+        btnConnect = Button(window, text="Connect", command=self.connect)
 
-btnStart = Button(window, text="Start", command=start_Collecting)
+        btnConnect.place(x=windowX / 2, y=windowY - 30)
 
-btnStart.place(x=10, y=windowY - 30)
-
-btnConnect = Button(window, text="Connect", command=connect)
-
-btnConnect.place(x=windowX/2, y=windowY - 30)
+        window.mainloop()
 
 
-window.mainloop()
+    def openNewWindow(self, lbl):
+        lbl.configure(text="Warum klickst du auf Fertig?? Du bist doch noch nicht fertig!!! :(")
+
+
+    def start_Collecting(self, thread):
+        if connected:
+            thread.start()
+
+
+    def finish_Collecting(self, thread):
+        if connected:
+            thread.join(1)
+            c.stop_collecting()
+            print("Alles fertig!")
+
+
+    def thread_work(self):
+        if connected:
+            while (1):
+                c.start_collecting()
+
+
+    def connect(self):
+        found_eyetrackers = tr.find_all_eyetrackers()
+
+        if len(found_eyetrackers) == 0:
+            print("No eyetrackers connected")
+            # exit()
+        else:
+            my_eyetracker = found_eyetrackers[0]
+            print("Address: " + my_eyetracker.address)
+            print("Model: " + my_eyetracker.model)
+            print("Name (It's OK if this is empty): " + my_eyetracker.device_name)
+            print("Serial number: " + my_eyetracker.serial_number)
+            global eyetracker
+            eyetracker = et.Eyetracker(my_eyetracker)
+            global connected
+            connected = True
+
+
+
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import src.UI.Gui as g
+
+class main:
+    def main(self):
+        gui = g.GUI()
+        gui.run()
+
+    if __name__ == "__main__": main(self)
