3515ae1aac09c73a88f2f22ce250445fb669cc03
Alexander Schorn <Alexander.Schorn@gmx.net>
2019-06-13 11:39:40 +0200

    Adjust pdfClass

--- src/util/pdfConverter.py
+++ src/util/pdfConverter.py
@@ -3,10 +3,12 @@ import tempfile
 # settings -> project interpreter -> '+* seach for 'pdf2image' and install
 from pdf2image import convert_from_path
 
-def convert(input_dir):
-    output_dir = os.path.join('src', 'static', 'img')
-    images = []
-    with tempfile.TemporaryDirectory() as path:
-        print('path: ', path)
-        images = convert_from_path(input_dir, output_folder=output_dir, fmt='jpeg')
-    return images
+class PdfConverter:
+
+    def convert(self, input_dir):
+        output_dir = os.path.join('src', 'static', 'img')
+        images = []
+        with tempfile.TemporaryDirectory() as path:
+            print('path: ', path)
+            images = convert_from_path(input_dir, output_folder=output_dir, fmt='jpeg')
+        return images
