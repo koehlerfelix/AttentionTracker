d035bb0f24902380dd36bc2f0ec99eca16b9e992
Alexander Schorn <Alexander.Schorn@gmx.net>
2019-06-16 15:36:24 +0200

    Add image to canvas size still buggy

--- src/UI/dashboard.py
+++ src/UI/dashboard.py
@@ -18,31 +18,44 @@ class Dashboard(tk.Toplevel):
 
         self.configure(background='#111111')
 
+
+        # create frame
         frame = Frame(self, borderwidth=1, background='#1E1E1E')
         frame.pack(fill=BOTH, expand=True)
 
-        # drawing circles
-        self.__canvas = Canvas(frame, width=window_x - 30, height=window_y - 130, background='white')
+        # making canvas
+        self.__canvas = Canvas(frame, width=window_x - 130, height=window_y - 230, background='white')
+        self.__canvas.pack(expand='True')
+        self.__canvas.update()
 
-        self.__label = Label(frame, width=220, height=45, background='white')
-
+        img = PhotoImage(file="images/Beispiel.png")
+        self.__canvas.create_image(20, 20, anchor=NW, image = img)
+        self.__canvas.update()
+        # self.__label = Label(frame, width=220, height=45, background='white')
         # self.__label.pack(expand='True')
 
+
+
+        # Button actions
         self.btn_finish = Button(self, text="finish", width=15, bg='white', command=self.close_window)
         self.btn_finish.pack(side="right", padx=5, pady=5)
 
         self.btn_show = Button(self, text="show", width=15, bg='white',
-                               command=lambda: self.show_circles(self.__canvas, window_x, window_y))
+                               command=lambda: self.show_gaze_points(window_x, window_y))
         self.btn_show.pack(side="top", padx=5, pady=5)
 
-        self.__canvas.pack(expand='True')
 
     def close_window(self):
         self.destroy()
 
-    def show_circles(self, canvas, window_x, window_y):
-        for i in range(self.__gaze_data_list.__sizeof__()):
-            canvas.create_text(self.__gaze_data_list[i][0] * window_x, self.__gaze_data_list[i][1] * window_y, text="x")
-            canvas.update
 
+    def show_gaze_points(self, window_x, window_y):
+
+        img = PhotoImage(file="images/Beispiel.png")
+        img = img.subsample(2)
+        self.__canvas.create_image(0, 0, anchor=NW, image=img)
+
+        for i in range(self.__gaze_data_list.__sizeof__()):
+            self.__canvas.create_text(self.__gaze_data_list[i][0] * window_x, self.__gaze_data_list[i][1] * window_y, text="x")
+            self.__canvas.update
         print("finished printing")
