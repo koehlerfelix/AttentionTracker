7318d30ffe5d664ca6dc04519e4fdc2166d78b38
benja <benjamin.hosenfeld@web.de>
2019-07-11 15:27:17 +0200

    view times for whole pdf document and each page

--- src/UI/dashboard.py
+++ src/UI/dashboard.py
@@ -9,16 +9,18 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import math
 import src.UI.diagramms as diagramms
+import datetime
 
 
 class Dashboard(tk.Toplevel):
 
     # gaze data list hat 2 listen: [0] hat keine "NaN" werte [1] hat welche
 
-    def __init__(self, gaze_data_list, avg_pupil_size, pupil_data_list):
+    def __init__(self, gaze_data_list, avg_pupil_size, pupil_data_list, statistics):
         self.__gaze_data_list = gaze_data_list
         self.__pupil_data_list = pupil_data_list
         self.__avg_pupil_size = avg_pupil_size
+        statistics = statistics
 
         tk.Toplevel.__init__(self)
 
@@ -42,12 +44,16 @@ class Dashboard(tk.Toplevel):
         button_frame.pack(fill=X, side=BOTTOM)
 
         # heading and general information
+        view_time = statistics['end_time'] - statistics['start_time']
         top_summary = Text(top_frame, height=20, width=window_x, bg='#e8e6e6', bd=0)
         top_summary.tag_configure('heading', font=('Verdana', 20, 'bold'))
         top_summary.tag_configure('alert', foreground='#d90000')
         top_summary.insert(END, '\nAttention Summary\n', 'heading')
-        top_summary.insert(END, 'Overall time spent: \n')
-        top_summary.insert(END, 'Seems like attention is decreasing: \n')
+        top_summary.insert(END, 'Overall time spent: ' + str(view_time) + '\n')
+        # top_summary.insert(END, 'Seems like attention is decreasing: \n')
+        top_summary.insert(END, 'Shortest view time: ' + str(min(statistics['page_times'])) + ' seconds on page X\n')
+        top_summary.insert(END, 'Longest view time: ' + str(max(statistics['page_times'])) + ' seconds on page X\n')
+        # top_summary.insert(END, 'Average view time: ' + str(max(statistics['page_times'])) + ' seconds on page X\n')
         top_summary.config(state=DISABLED)
         top_summary.pack(side=TOP, padx=20)
 
